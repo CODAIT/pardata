339b9305fd7b220eff214e41f69988fa29afb88b
Hong Xu <hongx@ibm.com>
2020-12-16 16:55:19 -0800

    Add internal directory lock utility
    This would be used later to avoid conflicting activities by `Dataset`
    from multiple processes.

---
+++ pydax/_lock.py
@@ -0,0 +1,114 @@
+#
+# Copyright 2020 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"Directory lock."
+
+from contextlib import contextmanager
+import os
+import pathlib
+import threading
+from uuid import uuid4
+from typing import Iterator
+
+from . import _typing
+
+
+class DirectoryLock:
+    """Read/write lock for a directory.
+
+    A lock file is named as ``{read|write}.{pid}.{uuid}.lock``. This should be able to resolve all potential name
+    clashes. We put process ID here in case someone wants to figure out which process created the file.
+
+    As a reservation for future compatibility, this class reserves all files starting with ``read.`` and ends with
+    ``.lock`` for its own use.
+
+    :param directory: The directory where lock files would be put.
+    """
+
+    def __init__(self, directory: _typing.PathLike):
+        self._uuid: str = str(uuid4())
+        self._directory: pathlib.Path = pathlib.Path(directory)
+        self._thread_lock: threading.Lock = threading.Lock()
+
+    @property
+    def _lock_file_suffix(self) -> str:
+        "The suffix of the lock file."
+        return f'.{os.getpid()}.{self._uuid}.lock'
+
+    def lock(self, *, write: bool) -> bool:
+        """Lock the directory (create the lock file in the directory).
+
+        :param write: Whether this is a write lock or a read lock. A write lock excludes others from both reading and
+            writing, while a read lock only excludes writing. Multiple read locks can exist at the same time, but only
+            one write lock may exist at any single moment.
+        :return: True if lock succeeds, False if fails. This function does not throw exceptions because :meth:`.lock` is
+            also used for peeking whether the lock is obtainable.
+        """
+
+        lock_file = self._directory / f'{"write" if write else "read"}{self._lock_file_suffix}'
+
+        def does_read_lock_exist() -> bool:
+            return next(self._directory.glob("read.*.lock"), None) is not None
+
+        def does_write_lock_exist() -> bool:
+            return next(self._directory.glob("write.*.lock"), None) is not None
+
+        with self._thread_lock:
+            if write:  # write lock
+                if does_read_lock_exist() or does_write_lock_exist():
+                    return False
+                else:
+                    lock_file.touch(exist_ok=False)
+            else:  # read lock
+                if does_write_lock_exist():
+                    return False
+                else:
+                    lock_file.touch(exist_ok=False)
+
+        return True
+
+    def unlock(self) -> bool:
+        """Unlock the directory.
+
+        :return: True if unlock succeeds, False if there is no lock to remove. This function does not throw exceptions
+            because it is commonplace to call :meth:`.unlock` from multiple locations and we consider the situation
+            where the lock has been removed as an expected usage.
+        """
+        with self._thread_lock:
+            lock_exists: bool = False
+            write_lock_file = self._directory / f'write{self._lock_file_suffix}'
+            if write_lock_file.exists():
+                write_lock_file.unlink()
+                lock_exists = True
+            read_lock_file = self._directory / f'read{self._lock_file_suffix}'
+            if read_lock_file.exists():
+                read_lock_file.unlink()
+                lock_exists = True
+            return lock_exists
+
+    @contextmanager
+    def locking(self, *, write: bool) -> Iterator[bool]:
+        """Same as :meth:`.lock`, but used in ``with`` statements.
+
+        Example:
+
+        .. code-blocks:: python
+
+           with some_lock.locking(write=True):
+               # do the work ...
+        """
+        yield self.lock(write=write)
+        self.unlock()
---
+++ tests/test_lock.py
@@ -0,0 +1,103 @@
+#
+# Copyright 2020 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+import pytest
+
+from pydax._lock import DirectoryLock
+
+
+class TestDirectoryLock:
+    "Test :class:`DirectoryLock`."
+
+    @staticmethod
+    def _ensure_lock_unlock_succeeds(directory: os.PathLike, write: bool):
+        "A utility function that ensures some locks and unlocks should succeed."
+        lock = DirectoryLock(directory)
+        prefix = 'write' if write else 'read'
+        assert lock.lock(write=write) is True
+        assert (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+        assert lock.unlock() is True
+        assert not (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+
+        with lock.locking(write=write) as succeed:
+            assert (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+            assert succeed is True
+        assert not (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+
+    @staticmethod
+    def _ensure_lock_fails(directory: os.PathLike, write: bool):
+        "A utility function that ensures some locks should fail."
+        lock = DirectoryLock(directory)
+        prefix = 'write' if write else 'read'
+        assert lock.lock(write=write) is False
+        assert not (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+
+        with lock.locking(write=write) as succeed:
+            assert succeed is False
+            assert not (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+        assert not (directory / f'{prefix}{lock._lock_file_suffix}').exists()
+
+    @staticmethod
+    def _ensure_unlock_fails(directory: os.PathLike):
+        "A utility function that ensures some unlocks should fail."
+        lock = DirectoryLock(directory)
+        assert lock.unlock() is False
+
+    def test_lock_empty(self, tmp_path):
+        "Test lock in an empty directory. Unlocking without locking should also fail."
+        for write in (True, False):
+            self._ensure_lock_unlock_succeeds(tmp_path, write)
+
+        self._ensure_unlock_fails(tmp_path)
+
+    @pytest.mark.parametrize('lock_file_list',
+                             [
+                                 # One write lock
+                                 ('write.ding.dong.lock',),
+                                 # Two write locks
+                                 ('write.dong.ding.lock', 'write.777.lock'),
+                                 # One write lock and one read lock
+                                 ('write.ding.dong.lock', 'read.ibm-codait.lock'),
+                             ])
+    def test_failing_write_lock(self, tmp_path, lock_file_list):
+        "Test locking when other write locks are present. Unlocking without locking should also fail."
+
+        for f in lock_file_list:
+            (tmp_path / f).touch()
+
+        for write in (True, False):
+            self._ensure_lock_fails(tmp_path, write)
+
+        self._ensure_unlock_fails(tmp_path)
+
+    @pytest.mark.parametrize('lock_file_list',
+                             [
+                                 # One read lock
+                                 ('read.ding.dong.lock',),
+                                 # Two read locks
+                                 ('read.dong.ding.lock', 'read.777.lock'),
+                             ])
+    def test_failing_read_lock(self, tmp_path, lock_file_list):
+        "Test locking when other read locks are present. Unlocking without locking should also fail."
+
+        for f in lock_file_list:
+            (tmp_path / f).touch()
+
+        self._ensure_lock_fails(tmp_path, write=True)
+        self._ensure_lock_unlock_succeeds(tmp_path, write=False)
+        self._ensure_unlock_fails(tmp_path)
