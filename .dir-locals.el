6e61b83679d97d54257c84d0e1f7597e6234f427
Hong Xu <hongx@ibm.com>
2021-05-26 23:04:15 -0700

    Add recommended Emacs configuration

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 120)))
+ (python-mode . ((flycheck-checker . python-flake8)
+                 (flycheck-flake8rc . \.flake8))))
