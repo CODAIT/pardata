911252cf6c4001aaf8a27496d678b00775460b11
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 20:52:35 +0000

    Adding tarfile member sanitization to extractall()

--- pardata/_dataset.py
+++ pardata/_dataset.py
@@ -157,7 +157,26 @@ class Dataset:
                 # We do not specify 'utf-8' here to match the default encoding used by the OS, which also likely
                 # uses this encoding for accessing the filesystem.
                 json.dump(members, f, indent=2)
-            tar.extractall(path=self._data_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self._data_dir)
 
     def _extract_as_zip(self, archive_fp: typing_.PathLike) -> None:
         """Extract ``archive_fp`` as tar. Raise the :exception:`zipfile.BadZipFile` object raised by
